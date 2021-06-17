dac343ecfa28e04ca4481d62f60c924f36947d1b
mari107 <62844176+MARI107@users.noreply.github.com>
2021-06-18 00:30:42 +0900

    スタイル設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { Button, ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router-dom";
+
 
 import theme from "./theme/theme";
 
 export default function App() {
   return (
     <ChakraProvider theme={theme}>
-      <BrowserRouter>
-        <Button colorScheme="teal">ボタン</Button>
-        <p>aaaaa</p>
-      </BrowserRouter>
+      <Button colorScheme="teal">ボタン</Button>
+      <p>aaaaa</p>
     </ChakraProvider>
   );
 }
